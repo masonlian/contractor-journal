b7e15f09ce2e595d213dddac647b92a5780bd5a7
masom <p88126123@gmail.com>
2025-06-14 16:05:15 +0800

    完成定義現階段需要的資料庫

---
+++ src/main/resources/sql/note_calendar_generator.sql
@@ -0,0 +1,15 @@
+INSERT INTO note_calender (calendar_date, is_weekend)
+
+WITH RECURSIVE date_gen (calendar_date) AS (
+    SELECT DATE('2025-06-01') AS calendar_date
+    UNION ALL
+    SELECT DATE_ADD(calendar_date, INTERVAL 1 DAY) AS calendar_date
+    FROM date_gen
+    WHERE calendar_date < DATE('2026-06-01')
+)
+
+SELECT
+    calendar_date,
+    DAYOFWEEK(calendar_date) IN (1, 7) AS is_weekend
+FROM date_gen;
+
